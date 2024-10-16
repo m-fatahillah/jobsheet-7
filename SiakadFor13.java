c7892245a60186a9cfb3d4111ae249837007d57a
m-fatahillah <fatahcrown789@gmail.com>
2024-10-16 21:33:23 +0700

    percobaan 1

--- SiakadFor13.java
+++ SiakadFor13.java
@@ -4,6 +4,8 @@ public class SiakadFor13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100; 
+        int lulus = 0, tidaklulus = 0;
+
 
         for (int i = 1;i <=10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -14,10 +16,18 @@ public class SiakadFor13 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai > 60) {
+                lulus ++;
+            } else {
+                tidaklulus++;
+            }
+            
 
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidaklulus);
         sc.close();
     }
 }
